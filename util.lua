00e3ce3b85d30e49e7a593c5943fab1f102d69f9
philer <philipp.miller@t-online.de>
2018-12-03 21:32:33 +0100

    util cleanup

--- util.lua
+++ util.lua
@@ -4,14 +4,6 @@
 
 local _memoization_clearers = {}
 
-function reset_data(update_count)
-    for _, memclear in ipairs(_memoization_clearers) do
-        if update_count % memclear[1] == 0 then
-            memclear[2]()
-        end
-    end
-end
-
 function memoize(delay, fn)
     if fn == nil then
         fn = delay
@@ -30,18 +22,58 @@ function memoize(delay, fn)
     end
 end
 
-function clamp(min, max, val)
-    return math.max(min, math.min(val, max))
+function reset_data(update_count)
+    for _, memclear in ipairs(_memoization_clearers) do
+        if update_count % memclear[1] == 0 then
+            memclear[2]()
+        end
+    end
 end
 
---- array based functions ---
 
-function array(iter)
-    local arr = {}
-    for item in iter do
-        table.insert(arr, item)
+-- class creation helper - takes a constructor as the only argument
+function class(init)
+    if init == nil then
+        init = function () return {} end
     end
-    return arr
+    local cls = setmetatable({}, {
+        __call = function (cls, ...)
+            return setmetatable(init(...), cls)
+        end,
+    })
+    cls.__index = cls
+    return cls
+end
+
+
+-- circular queue implementation
+CycleQueue = class(function (length)
+    return {length = length, latest = 1}
+end)
+
+function CycleQueue:head()
+    return self[self.latest % self.length + 1] or 0
+end
+
+function CycleQueue:add(item)
+    self.latest = self.latest % self.length + 1
+    self[self.latest] = item
+end
+
+function CycleQueue:map(fn)
+    for i = self.latest + 1, self.length do
+        fn(self[i] or 0, i - self.latest)
+    end
+    for i = 1, self.latest do
+        fn(self[i] or 0, self.length - self.latest + i)
+    end
+end
+
+
+-- general utility functions --
+
+function clamp(min, max, val)
+    return math.max(min, math.min(val, max))
 end
 
 function map(fn, iter)
@@ -73,104 +105,4 @@ function avg(arr)
         acc = acc + nr
     end
     return acc / #arr
-end
-
-
---- iterator based functions ---
-
-function iter(arr)
-    local i = 0
-    return function()
-        i = i + 1
-        return arr[i]
-    end
-end
-
-function imap(fn, iter)
-    return function()
-        return fn(iter())
-    end
-end
-
-function ireduce(fn, init, iter)
-    for item in iter do
-        init = fn(init, item)
-    end
-    return init
-end
-
-function irange(start, stop, step)
-    step = step or 1
-    local current = start - step
-    return function()
-        current = current + step
-        if current > stop then
-            return nil
-        end
-        return current
-    end
-end
-
-function iavg(iter)
-    local acc, count = 0, 0
-    for nr in iter do
-        acc = acc + nr
-        count = count + 1
-    end
-    return acc / count
-end
-
-
---- variadic functions ---
-
-function navg(...)
-    return sum(unpack(arg)) / #arg
-end
-
-function nsum(...)
-    local result = 0
-    for _, v in ipairs(arg) do
-        result = result + v
-    end
-    return result
-end
-
-
--- class creation helper - takes a constructor as the only argument
-function class(init)
-    if init == nil then
-        init = function () return {} end
-    end
-    local cls = setmetatable({}, {
-        __call = function (cls, ...)
-            return setmetatable(init(...), cls)
-        end,
-    })
-    cls.__index = cls
-    return cls
-end
-
-
--- circular queue implementation --
-
-CycleQueue = class(function (length)
-    return {length = length, latest = 1}
-end)
-
-function CycleQueue:head()
-    return self[self.latest % self.length + 1] or 0
-end
-
-function CycleQueue:add(item)
-    self.latest = self.latest % self.length + 1
-    self[self.latest] = item
-end
-
-function CycleQueue:map(fn)
-    for i = self.latest + 1, self.length do
-        fn(self[i] or 0, i - self.latest)
-    end
-    for i = 1, self.latest do
-        fn(self[i] or 0, self.length - self.latest + i)
-    end
 end

6516f39b3fe3337795d89666cec7a897d7681ed7
philer <philipp.miller@t-online.de>
2019-12-01 19:36:25 +0100

    Added StaticText Widget. Also added abstract base class Text for StaticText and TextLine. Text Widgets now support font slant and font weight options.

--- src/widget.lua
+++ src/widget.lua
@@ -443,58 +443,105 @@ function Frame:render_background(cr)
     end
 end
 
+--- Common (abstract) base class for `StaticText` and `TextLine`.
+-- @type Text
+local Text = util.class(Widget)
+w.Text = Text
 
---- Draw a single line changeable of text.
--- Use this widget for text that will be updated on each cycle.
--- @type TextLine
-local TextLine = util.class(Widget)
-w.TextLine = TextLine
+local write_aligned = {left = ch.write_left,
+                       center = ch.write_centered,
+                       right = ch.write_right}
 
 --- @tparam table args table of options
 -- @tparam ?string args.align "left" (default), "center" or "right"
--- @tparam ?string args.font_family
--- @tparam ?number args.font_size
+-- @tparam[opt=w.default_font_family] ?string args.font_family
+-- @tparam[opt=w.default_font_size] ?number args.font_size
+-- @tparam[opt=CAIRO_FONT_WEIGHT_NORMAL] ?cairo_font_slant_t args.font_slant
+-- @tparam[opt=CAIRO_FONT_SLANT_NORMAL] ?cairo_font_weight_t args.font_weight
 -- @tparam ?{number,number,number,number} args.color (default: `default_text_color`)
-function TextLine:init(args)
-    self.align = args.align or "left"
-    self.font_family = args.font_family or w.default_font_family
-    self.font_size = args.font_size or w.default_font_size
-    self.color = args.color or w.default_text_color
+function Text:init(args)
+    assert(getmetatable(self) ~= Text, "Cannot instanciate class Text directly.")
+    self._align = args.align or "left"
+    self._font_family = args.font_family or w.default_font_family
+    self._font_size = args.font_size or w.default_font_size
+    self._font_slant = args.font_slant or CAIRO_FONT_WEIGHT_NORMAL
+    self._font_weight = args.font_weight or CAIRO_FONT_SLANT_NORMAL
+    self._color = args.color or w.default_text_color
 
-    local write_fns = {left = ch.write_left,
-                       center = ch.write_centered,
-                       right = ch.write_right}
-    self._write_fn = write_fns[self.align]
+    self._write_fn = write_aligned[self._align]
 
-    local extents = ch.font_extents(self.font_family, self.font_size)
-    self.height = extents.height + 1
-    local line_spacing = extents.height - (extents.ascent + extents.descent)
     -- try to match conky's line spacing:
-    self._baseline_offset = extents.ascent + 0.5 * line_spacing + 1
+    local font_extents = ch.font_extents(self._font_family, self._font_size,
+                                         self._font_slant, self._font_weight)
+    self._line_height = font_extents.height + 1
+
+    local line_spacing = font_extents.height - (font_extents.ascent + font_extents.descent)
+    self._baseline_offset = font_extents.ascent + 0.5 * line_spacing + 1
+end
+
+function Text:layout(width)
+    if self._align == "center" then
+        self._x = 0.5 * width
+    elseif self._align == "left" then
+        self._x = 0
+    else  -- self._align == "right"
+        self._x = width
+    end
+end
+
+
+--- Draw some unchangeable text.
+-- Use this widget for text that will never be updated.
+-- @type StaticText
+local StaticText = util.class(Text)
+w.StaticText = StaticText
+
+--- @string text Text to be displayed.
+-- @tparam ?table args table of options, see `Text:init`
+function StaticText:init(text, args)
+    Text.init(self, args or {})
+
+    self._lines = {}
+    local _, line_count = text:gsub("[^\n]*", function(line)
+        table.insert(self._lines, line)
+    end)
+    self.height = line_count * self._line_height
+end
+
+function StaticText:render_background(cr)
+    ch.set_font(cr, self._font_family, self._font_size, self._font_slant,
+                    self._font_weight)
+    cairo_set_source_rgba(cr, unpack(self._color))
+    for i, line in ipairs(self._lines) do
+        local y = self._baseline_offset + (i - 1) * self._line_height
+        self._write_fn(cr, self._x, y, line)
+    end
+end
+
+
+--- Draw a single line of changeable text.
+-- Text line can be updated on each cycle via `set_text`.
+-- @type TextLine
+local TextLine = util.class(Text)
+w.TextLine = TextLine
+
+--- @tparam table args table of options, see `Text:init`
+function TextLine:init(args)
+    Text.init(self, args)
+    self.height = self._line_height
 end
 
 --- Update the text line to be displayed.
 -- @string text
 function TextLine:set_text(text)
-    self.text = text
-end
-
-function TextLine:layout(width)
-    if self.align == "center" then
-        self._x = 0.5 * width
-    elseif self.align == "left" then
-        self._x = 0
-    else  -- self.align == "right"
-        self._x = width
-    end
+    self._text = text
 end
 
 function TextLine:render(cr)
-    cairo_select_font_face(cr, self.font_family, CAIRO_FONT_SLANT_NORMAL,
-                                                 CAIRO_FONT_WEIGHT_NORMAL)
-    cairo_set_font_size(cr, self.font_size)
-    cairo_set_source_rgba(cr, unpack(self.color))
-    self._write_fn(cr, self._x, self._baseline_offset, self.text)
+    ch.set_font(cr, self._font_family, self._font_size, self._font_slant,
+                    self._font_weight)
+    cairo_set_source_rgba(cr, unpack(self._color))
+    self._write_fn(cr, self._x, self._baseline_offset, self._text)
 end
 
 
@@ -1196,9 +1243,7 @@ function GpuTop:update()
 end
 
 function GpuTop:render(cr)
-    cairo_select_font_face(cr, self._font_family, CAIRO_FONT_SLANT_NORMAL,
-                                                 CAIRO_FONT_WEIGHT_NORMAL)
-    cairo_set_font_size(cr, self._font_size)
+    ch.set_font(cr, self._font_family, self._font_size)
     cairo_set_source_rgba(cr, unpack(self._color))
     cairo_set_antialias(cr, CAIRO_ANTIALIAS_NONE)
 

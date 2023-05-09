fc93a1e85a4349820648116a7187e46eb5436f8e
Simon Lees <sflees@suse.de>
2023-05-09 11:10:09 +0930

    Formatting fixes
    * Don't line up requires
    * Don't worry about aliasing module names

--- layout.lua
+++ layout.lua
@@ -7,22 +7,13 @@ package.path = script_dir .. "?.lua;" .. package.path
 local conkyrc = require('conkyrc')
 local polycore = require('src/polycore')
 local data = require('src/data')
-local core  = require('src/widgets/core')
-local cpu   = require('src/widgets/cpu')
+local core = require('src/widgets/core')
+local cpu = require('src/widgets/cpu')
 local drive = require('src/widgets/drive')
-local gpu   = require('src/widgets/gpu')
-local mem   = require('src/widgets/memory')
-local net   = require('src/widgets/network')
+local gpu = require('src/widgets/gpu')
+local mem = require('src/widgets/memory')
+local net = require('src/widgets/network')
 local text  = require('src/widgets/text')
-
-local Frame, Filler, Rows, Columns = core.Frame, core.Filler,
-                                          core.Rows, core.Columns
-local Cpu, CpuFrequencies = cpu.Cpu, cpu.CpuFrequencies
-local Drive = drive.Drive
-local Gpu, GpuTop = gpu.Gpu, gpu.GpuTop
-local MemoryGrid = mem.MemoryGrid
-local Network = net.Network
-local TextLine = text.TextLine
 
 -- Draw debug information
 DEBUG = false
@@ -37,7 +28,7 @@ function polycore.setup()
     -- Write fan speeds. This requires lm_sensors to be installed.
     -- Run `sensonrs` to see if any fans are reported. If not, remove
     -- this section and the corresponding line below.
-    local fan_rpm_text = TextLine{align="center", color=secondary_text_color}
+    local fan_rpm_text = text.TextLine{align="center", color=secondary_text_color}
     fan_rpm_text.update = function(self)
         local fans = data.fan_rpm()
         self:set_text(table.concat{fans[1], " rpm   ·   ", fans[2], " rpm"})
@@ -45,7 +36,7 @@ function polycore.setup()
 
     -- Write individual CPU core temperatures as text.
     -- This also relies on lm_sensors.
-    local cpu_temps_text = TextLine{align="center", color=secondary_text_color}
+    local cpu_temps_text = text.TextLine{align="center", color=secondary_text_color}
     cpu_temps_text.update = function(self)
         local cpu_temps = data.cpu_temperatures()
         self:set_text(table.concat(cpu_temps, " · ") .. " °C")
@@ -53,7 +44,7 @@ function polycore.setup()
 
     -- Write individual CPU core temperatures as text.
     -- This also relies on lm_sensors.
-    local gpu_power_text = TextLine{align="right", font_size=10.1}
+    local gpu_power_text = text.TextLine{align="right", font_size=10.1}
     gpu_power_text.update = function(self)
         local fans = data.fan_rpm()
         local gpu_power_draw = string.format("%.0f", data.gpu_power_draw())
@@ -63,41 +54,41 @@ function polycore.setup()
     local widgets = {
         fan_rpm_text,  -- see above
         cpu_temps_text,  -- see above
-        Filler{height=3},
+        core.Filler{height=3},
 
         -- Adjust the CPU core count to your system.
         -- Requires lm_sensors for CPU temperatures.
-        Cpu{cores=8, inner_radius=28, gap=5, outer_radius=57},
-        Filler{height=7},
-        CpuFrequencies{cores=8, min_freq=0.75, max_freq=4.3},
-        Filler{height=129},
+        cpu.Cpu{cores=8, inner_radius=28, gap=5, outer_radius=57},
+        core.Filler{height=7},
+        cpu.CpuFrequencies{cores=8, min_freq=0.75, max_freq=4.3},
+        core.Filler{height=129},
 
         -- See also widget.MemoryBar
-        MemoryGrid{rows=5},
-        Filler{height=78},
+        mem.MemoryGrid{rows=5},
+        core.Filler{height=78},
 
         -- Requires `nvidia-smi` to be installed. Does not work for AMD GPUs.
         gpu_power_text,  -- see above
-        Filler{height=2},
-        Gpu(),
-        Filler{height=1},
-        GpuTop{lines=5, color=secondary_text_color},
-        Filler{height=66},
+        core.Filler{height=2},
+        gpu.Gpu(),
+        core.Filler{height=1},
+        gpu.GpuTop{lines=5, color=secondary_text_color},
+        core.Filler{height=66},
 
         -- Adjust the interface name for your system. Run `ifconfig` to find
         -- out yours. Common names are "eth0" and "wlan0".
-        Network{interface="enp34s0u1u3u4", downspeed=5 * 1024, upspeed=1024,
+        net.Network{interface="enp34s0u1u3u4", downspeed=5 * 1024, upspeed=1024,
                        graph_height=22},
-        Filler{height=34},
+        core.Filler{height=34},
 
         -- Mount paths. Devices that aren't mounted will not be rendered until
         -- they appear. That way external drives can be displayed automatically.
-        Drive("/"),
-        Drive("/mnt/blackstor"),
-        Drive("/mnt/bluestor"),
-        Filler(),
+        drive.Drive("/"),
+        drive.Drive("/mnt/blackstor"),
+        drive.Drive("/mnt/bluestor"),
+        core.Filler(),
     }
-    local root = widget.Frame(widget.Rows(widgets), {
+    local root = core.Frame(core.Rows(widgets), {
         padding={108, 9, 10, 10},
         border_color={0.8, 1, 1, 0.05},
         border_width = 1,
